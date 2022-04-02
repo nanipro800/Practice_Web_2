cee18efe2301d8338f840c2fe26303a015cc4e4b
nanipro800 <53618095+nanipro800@users.noreply.github.com>
2022-04-02 17:02:56 +0700

    push

---
+++ Employee-Thymeleaf-CRUD/src/main/java/net/javaguides/springboot/EmployeeThymeleafCrudApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeThymeleafCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeThymeleafCrudApplication.class, args);
+	}
+
+}
