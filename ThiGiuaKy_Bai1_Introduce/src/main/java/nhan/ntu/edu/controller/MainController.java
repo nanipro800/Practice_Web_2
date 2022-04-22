bf963be70b8749d2d0902c606ddd1d99ef8bd8ae
nanipro800 <53618095+nanipro800@users.noreply.github.com>
2022-04-22 17:18:39 +0700

    Fix

--- ThiGiuaKy_Bai1_Introduce/src/main/java/nhan/ntu/edu/controller/MainController.java
+++ ThiGiuaKy_Bai1_Introduce/src/main/java/nhan/ntu/edu/controller/MainController.java
@@ -1,16 +1,21 @@
 package nhan.ntu.edu.controller;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class MainController {
 	// Được tiêm vào (inject) từ application.properties.
 
-		@RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
-		public String index() {
-			return "index";
-		}
+
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping("/contact")
+	public String contact() {
+		return "contact";
+	}
 		
 }
